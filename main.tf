51fd9e0eb28142258ceaa6eece0b79ae981f8203
Per Alonso <per@oktav.se>
2021-10-12 15:16:24 +0200

    Added some trial basic resources

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
-provider "openstack" {}
+terraform {
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.44.0"
+    }
+  }
+}
+
+module "security_group" {
+  source        = "./modules/"
+  secgroup_name = "group4-secgroup"
+}
--- modules/client/main.tf
+++ modules/client/main.tf
@@ -1,18 +1,27 @@
+terraform {
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.44.0"
+    }
+  }
+}
+
 data "template_file" "cloud_init_script" {
   template = file("/path/to/cloud/init/script")
 }
 
 resource "openstack_compute_instance_v2" "client" {
   count = var.n_o_client
-  name = format("${var.client_prefix}_%d", count.index)
+  name  = format("${var.client_prefix}_%d", count.index)
 
   network {
     name = var.virtual_network_name
   }
 
-  image_id = var.image_id
-  user_data = data.template_file.cloud_init_script.rendered
+  image_id        = var.image_id
+  user_data       = data.template_file.cloud_init_script.rendered
   security_groups = [var.secgroup_name]
-  flavor_name = var.client_flavor_name
-  key_pair = var.client_key_pair
+  flavor_name     = var.client_flavor_name
+  key_pair        = var.client_key_pair
 }
