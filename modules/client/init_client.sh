e6a34fe677808c2ad0e86cd83e02e1fcc54cda1c
Per Alonso <Per.Alonso.3561@student.uu.se>
2021-10-21 10:58:43 +0200

    Added automation scripts etc. for the client

---
+++ modules/client/init_client.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+sudo apt update -y
+sudo apt install -y docker-compose git-lfs
+
+cd ~
+
+git clone https://github.com/scaleoutsystems/examples.git
+
+
+cd examples/mnist-keras/
+cp ~/extra-hosts-client.yaml ./extra-hosts.yaml
+cp ~/Dockerfile ./Dockerfile
+curl -k "https://${reducer_ip}:8090/config/download" > client.yaml
+
+sudo docker build --network=host . -t mnist-client:latest
+
+sudo -E docker network create fedn_default
+sudo -E docker-compose -f docker-compose.yaml -f private-network.yaml -f extra-hosts.yaml up -d
