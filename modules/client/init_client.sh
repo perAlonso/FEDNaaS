9a29b64d7542af6c40cbaeea0d02819128c42d75
Per Alonso <Per.Alonso.3561@student.uu.se>
2021-10-22 11:28:15 +0200

    Added copy for data in client

--- modules/client/init_client.sh
+++ modules/client/init_client.sh
@@ -15,5 +15,8 @@ curl -k "https://${reducer_ip}:8090/config/download" > client.yaml
 
 sudo docker build --network=host . -t mnist-client:latest
 
+sudo cp data/mnist.npz data/clients/1/
+sudo cp data/mnist.npz data/clients/0/
+
 sudo -E docker network create fedn_default
 sudo -E docker-compose -f docker-compose.yaml -f private-network.yaml -f extra-hosts.yaml up -d
